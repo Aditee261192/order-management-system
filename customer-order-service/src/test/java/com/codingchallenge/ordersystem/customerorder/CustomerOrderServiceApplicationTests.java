ef39cd6c648cf24258a95b12ce10d61617874bc1
Aditee Kale <aditi.kale.san@gmail.com>
2026-05-13 10:53:08 +0200

    Adding openAPI specification for createOrder for Customer Order service

---
+++ customer-order-service/src/test/java/com/codingchallenge/ordersystem/customerorder/CustomerOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingchallenge.ordersystem.customerorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerOrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
